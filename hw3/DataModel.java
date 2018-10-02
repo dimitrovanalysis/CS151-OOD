17b9c248341f2f13621ad8f415a7e2f71acd1288
Konstantin <2dimitrovkonstantin@gmail.com>
2018-10-01 21:38:14 -0700

    Add files via upload
    ALL class files taken from cayhorstman and modified to create a smooth zoom in/ out transition

---
+++ hw3/DataModel.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class DataModel {
+	private int Wicon;
+	ArrayList<ChangeListener> listener;
+	
+	public DataModel(int width) {
+		Wicon = width;
+		listener = new ArrayList<ChangeListener>();
+	}
+	
+	/**
+	    Attach a listener to the Model
+	    @param c the listener
+	 */
+	 public void attach(ChangeListener c)
+	 {
+	    listener.add(c);
+	 }
+	 
+	 public int getIconWidth() {
+		 return Wicon;
+	 }
+	 
+	 public ChangeListener changeListener() {
+		 return listener.get(0);
+	 }
+	 
+	 public void update(int width) {
+		 Wicon = width;
+		 for (int i = 0; i < listener.size(); i++)
+	      {
+	         listener.get(i).stateChanged(new ChangeEvent(this));
+	      }
+	 }
+
+}
