279b6dd304f5c7b7f6547cd1d6a56a1193f9998d
Konstantin <2dimitrovkonstantin@gmail.com>
2018-10-14 20:43:10 -0700

    Add files via upload

---
+++ hw5/hw5/LineItem.java
@@ -0,0 +1,23 @@
+/**
+   A line item in an invoice.
+*/
+public interface LineItem
+{
+   /**
+      Gets the price of this line item.
+      @return the price
+   */
+   double getPrice();
+   /**
+      Gets the description of this line item.
+      @return the description
+   */   
+   String toString();
+   
+   /**
+    * want to return quantity
+    * @return the quantity
+    */
+   int getQuantity();
+   void increaseQuantity();
+}
---
+++ hw5/hw5/SimpleFormatter.java
@@ -0,0 +1,24 @@
+/**
+   A simple invoice formatter.
+*/
+public class SimpleFormatter implements InvoiceFormatter
+{
+   public String formatHeader()
+   {
+      total = 0;
+      return "     I N V O I C E\n\n\n";
+   }
+
+   public String formatLineItem(LineItem item)
+   {
+      total += item.getPrice() * item.getQuantity();
+   return item.toString() + " Price: " + item.getPrice() + " Quantity: " + item.getQuantity();
+   }
+
+   public String formatFooter()
+   {
+      return (String.format("\n\nTOTAL DUE: $%.2f\n", total));
+   }
+
+   private double total;
+}
